df3e4c9333fa0d9b488092270845358b2ddf3935
Antoni Baum <antoni.baum@protonmail.com>
2020-10-21 00:46:28 +0200

    Tweaks

--- trbot_google.py
+++ trbot_google.py
@@ -14,22 +14,30 @@ CHROME_OPTIONS = {}
 if CHROME_BINARY:
     CHROME_OPTIONS["binary"] = CHROME_BINARY
 
+DEFAULT_MSG = "Sorry, I have no answer for this."
+
+
 async def get_google_answer(message):
-    url_text = TAG_RE.sub("", message.content.replace("`", "").replace("!answer", "")).strip().replace(" ", "+")
+    url_text = (
+        TAG_RE.sub("", message.content.replace("`", "").replace("!answer", ""))
+        .strip()
+        .replace(" ", "+")
+    )
     service = services.Chromedriver()
-    browser = browsers.Chrome(**{"goog:chromeOptions":CHROME_OPTIONS})
+    browser = browsers.Chrome(**{"goog:chromeOptions": CHROME_OPTIONS})
     source = None
-    msg = "Sorry, I have no answer for this."
+    msg = DEFAULT_MSG
     try:
         async with get_session(service, browser) as session:
-            #await session.set_window_size(1920, 1080)
-            await session.get(f'https://www.google.com/search?hl=en&q={url_text}')
+            await session.get(f"https://www.google.com/search?hl=en&q={url_text}")
             a = None
             b = None
             try:
-                kp = await session.wait_for_element(1, 'div[class|="kp"')
-                a = await kp.get_element('div[aria-level="3"][role="heading"][data-attrid]')
-                a = await a.get_element('span')
+                kp = await session.wait_for_element(1, 'div[class|="kp"]')
+                a = await kp.get_element(
+                    'div[aria-level="3"][role="heading"][data-attrid]'
+                )
+                a = await a.get_element("span")
             except Exception as e:
                 if not isinstance(e, ArsenicTimeout):
                     traceback.print_exc()
@@ -39,12 +47,16 @@ async def get_google_answer(message):
                 pass
             else:
                 try:
-                    b = await session.wait_for_element(1, 'div[data-attrid="description"]')
+                    b = await session.wait_for_element(
+                        1, 'div[data-attrid="description"]'
+                    )
                 except Exception as e:
                     if not isinstance(e, ArsenicTimeout):
                         traceback.print_exc()
                     try:
-                        b = await session.wait_for_element(1, 'div[data-attrid^="kc"] span:not([class])')
+                        b = await session.wait_for_element(
+                            1, 'div[data-attrid^="kc"]:not([data-attrid*="image"]) span:not([class])'
+                        )
                     except Exception as e:
                         if not isinstance(e, ArsenicTimeout):
                             traceback.print_exc()
@@ -54,13 +66,18 @@ async def get_google_answer(message):
                     source = await b.get_attribute("outerHTML")
                     msg = ""
     except:
-        msg = "Sorry, I have no answer for this."
+        msg = DEFAULT_MSG
         traceback.print_exc()
     if not msg:
         if source == "NULL" or not source:
-            msg = "Sorry, I have no answer for this."
+            msg = DEFAULT_MSG
         else:
             soup = BeautifulSoup(source)
+            links = [
+                x["href"]
+                for x in soup.find_all("a", attrs={"href": True})
+                if "wikipedia" in x["href"]
+            ]
             str_lst = [x.strip() for x in soup.stripped_strings]
             if len(str_lst) > 1:
                 str_lst = str_lst[:-1]
@@ -68,7 +85,9 @@ async def get_google_answer(message):
                 str_lst.pop(0)
             elif len(str_lst) > 1:
                 str_lst[0] = f"**{str_lst[0]}**"
+            if links:
+                str_lst.append(links[0])
             msg = " ".join(str_lst)
     if not msg:
-        msg = "Sorry, I have no answer for this."
+        msg = DEFAULT_MSG
     return msg
